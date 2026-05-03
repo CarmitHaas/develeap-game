326bb8c7aa6fd7f4a5f2a903e1c40929180b081e
Carmit Haas <carmithaas@gmail.com>
2026-05-03 20:39:22 +0300

    wire Inbar.png portrait

--- characters.js
+++ characters.js
@@ -9,7 +9,7 @@ const CHARACTERS = {
   avi:    { name: "Avi",    color: "#5e348c", avatar: "👔", role: "CTO, Develeap",                 image: "Avi.png"   },
   maya:   { name: "Maya",   color: "#e01e5a", avatar: "M",  role: "Senior consultant",             image: "Maya.png"  },
   segev:  { name: "Segev",  color: "#2eb67d", avatar: "S",  role: "Senior DevOps",                 image: "Segev.png" },
-  inbar:  { name: "Inbar",  color: "#ecb22e", avatar: "I",  role: "Junior consultant @ FastShip" },
+  inbar:  { name: "Inbar",  color: "#ecb22e", avatar: "I",  role: "Junior consultant @ FastShip", image: "Inbar.png" },
   keren:  { name: "Keren",  color: "#4a154b", avatar: "K",  role: "PM @ FastShip",                 image: "Keren.png" },
   bingo:  { name: "Bingo",  color: "#f97316", avatar: "🐕", role: "Office dog",                    image: "Bingo.png" },
   yarden: { name: "Yarden", color: "#0e7490", avatar: "Y",  role: "Trainer @ bootcamp-23" },
